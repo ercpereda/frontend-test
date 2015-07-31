e39b05b86e68929154919512d2e3ee4772fdc5e2
Ernesto Rafael Cruz Pereda <ercp42@gmail.com>
2015-07-31 07:25:37 -0400

    change identation to 2 spaces

--- frontend/directives/er-color-palette-directive.js
+++ frontend/directives/er-color-palette-directive.js
@@ -1,23 +1,23 @@
 'use strict';
 
 erEventApp
-    .directive('erColorPalette',
-        function() {
-            return {
-                restrict: 'E',
-                replace: true,
-                templateUrl: '/templates/directives/er-color-palette.html',
-                scope: {
-                  enter: '&',
-                  leave: '&',
-                  click: '&'
-                },
-                controller: function($scope) {
-                    $scope.colors = ['dark', 'darkBrown', 'darkCrimson', 'darkViolet', 'darkMagenta',
-                        'darkCyan', 'darkCobalt', 'darkTeal', 'darkEmerald', 'darkGreen',
-                        'darkOrange', 'darkRed', 'darkPink', 'darkIndigo', 'darkBlue',
-                        'lightBlue', 'lightTeal', 'lightOlive', 'lightOrange', 'lightPink'];
-                }
-            }
+  .directive('erColorPalette',
+    function() {
+      return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/templates/directives/er-color-palette.html',
+        scope: {
+          enter: '&',
+          leave: '&',
+          click: '&'
+        },
+        controller: function($scope) {
+          $scope.colors = ['dark', 'darkBrown', 'darkCrimson', 'darkViolet', 'darkMagenta',
+            'darkCyan', 'darkCobalt', 'darkTeal', 'darkEmerald', 'darkGreen',
+            'darkOrange', 'darkRed', 'darkPink', 'darkIndigo', 'darkBlue',
+            'lightBlue', 'lightTeal', 'lightOlive', 'lightOrange', 'lightPink'];
         }
+      }
+    }
 );
