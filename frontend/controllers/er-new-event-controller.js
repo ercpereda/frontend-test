b08eeaaa025922a1e6460c84d44d0f8a49e9e781
Ernesto Rafael Cruz Pereda <ercp42@gmail.com>
2015-08-01 05:13:37 -0400

    add fields and bindings to the create event form

--- frontend/controllers/er-new-event-controller.js
+++ frontend/controllers/er-new-event-controller.js
@@ -2,6 +2,16 @@
 
 erEventApp.controller('erNewEventController',
     function($scope) {
-        $scope.imageUrl = '';
+        $scope.addDateTime = function (date, time) {
+            if (!$scope.event.dates)
+                $scope.event.dates = [];
+            $scope.event.dates.push(date + ' ' + time);
+            console.log($scope.event);
+        };
+
+        $scope.save = function (event) {
+            console.log($scope);
+            console.log(event);
+        };
     }
 );
